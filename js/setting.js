32ee69d49ab08a452c5806518443dd2d61579dcc
ryan5500 <ryan5500@gmail.com>
2013-12-14 14:56:30 +0900

    ロードするデータを品川区のものに設定

--- js/setting.js
+++ js/setting.js
@@ -10,8 +10,8 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'yokohama_budget';
-OpenSpending.year = '2012';
+OpenSpending.identifier = 'shinagawa_2013_start';
+OpenSpending.year = '2013';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
 
